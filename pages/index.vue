6398b587cc0c51d9e7e7c64951019c93c369f560
島袋雄太 <mrs1122ani@gmail.com>
2020-01-09 14:24:58 +0900

    background imageの貼り付け

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <v-app>
-    <v-content class="bg">   
-        <v-card class="mx-3 my-12 pb-5" max-width="374">
-          <v-card-title class="d-flex justify-center py-10">{{question}}</v-card-title>
-          <div class="mx-auto my-2 d-flex justify-center d-flex align-start flex-column">
-            <div class="mx-auto" v-for="(button,index) in btn" :key="index">
-              <v-btn width="230" height="50" rounded class="cyan accent-2 mb-8">{{button}}</v-btn>
-            </div>
-          </div>
-        </v-card>    
+    <v-content class="bg">      
     </v-content>
   </v-app>
 </template>
@@ -19,8 +11,8 @@
 export default {
   data: function(){
     return {
-    question: '好きな言語はなんですか？',
-    btn: ["C", "Java","Java script","PHP","ruby"]
+    question: ['好きな言語は？','好きな動物は？','好きな果物は？'],
+    choices:[["C", "Java","Java script","PHP","ruby"],["ゾウ","キリン","カバ","ライオン"],["リンゴ","バナナ","ブドウ","モモ"]]
     }
   }
 }
@@ -36,7 +28,7 @@ export default {
     top: 0;
     left: 0;
     background-size: cover;
-    background-image: url("~@/assets/foo.jpg");
+    background-image: url("~@/assets/images/foo.jpg");
   }
   
 </style>
